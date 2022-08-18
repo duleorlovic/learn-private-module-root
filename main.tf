c3438bd280dfc5b6b42889277949050687a630c7
Dusan Orlovic <dule@hey.com>
2022-08-18 19:15:22 +0200

    Update module path

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "duleorlovic/s3-webapp/aws"
+  version = "1.0.0"
+  # insert the 3 required variables here
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
 }
